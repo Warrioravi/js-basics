d881760396e77fe9d87c1ab0b6f8be03a12b1a45
Warrioravi <abhisinghraj2k18@gmail.com>
2021-07-25 21:57:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector(".name").addEventListener('click',(e)=>{
+    e.currentTarget.innerText="";
+})
+document.querySelector(".continue").addEventListener('click',(e)=>{
+    document.documentElement.requestFullscreen()
+    .then(()=>{
+        $(".userRegister").fadeOut("slow");
+        setTimeout(() => {
+            $(".bod").fadeOut("slow");
+        },8000);
+    })
+})
