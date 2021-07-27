7d5ea45660b8d1d6a8d9bf4867ad9b9315c9f894
Warrioravi <abhisinghraj2k18@gmail.com>
2021-07-27 20:49:56 +0530

    frame added

--- script.js
+++ script.js
@@ -16,27 +16,27 @@ $("#start").click(function(){
 })
 $("#vscode").click(function(){
     console.log("vs code clicked")
-    let newframe=createiframe("camera","https://github1s.com/Warrioravi/MySheet");
+    let newframe=createiframe("vscode","https://github1s.com/Warrioravi/MySheet");
     $("body").append(newframe);
 })
 
 
 function createiframe(name,url){
     let rv=document.createElement("div");
-    rv.innerHTML=` <div class="frametop">
+    rv.innerHTML=`<div class="frametop">
     <div class="framedetails">
-        <div class="frameicon"><img src="/assets/${name}.png" height="25px" width="20px" alt="" srcset=""></div>
-        <div class="framename">${name}</div>
+        <div class="frameicon"><img src="/assets/${name}.png" height="25px"></div>
+        <div class="framename">Vscode</div>
     </div>
     <div class="framecontrols">
-        <div class="frameminimize">_</div>
-        <div class="frameresize">MI</div>
-        <div class="frameclose">X</div>
+        <div class="frameminimize" style="padding-bottom: 10px;" >__</div>
+       
+        <div class="frameresize"><img src="/assets/minimize.png" height="20px" width="20px" alt="" srcset=""></div>
+        <div class="frameclose"><img src="/assets/close.png" height="14px" alt="" srcset=""></div>
     </div>
 </div>
 <div class="framecontent">
    <iframe id="gitframe" src="${url}" frameborder="0"></iframe>
-</div>
 </div>`;
 rv.classList.add("frame");
 rv.querySelector(".frameclose").addEventListener('click',()=>{
