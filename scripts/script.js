e08b2e16778aac7221d6b3793c7d0209be60797d
Warrioravi <abhisinghraj2k18@gmail.com>
2021-07-29 21:04:31 +0530

    removed tests

--- scripts/script.js
+++ scripts/script.js
@@ -167,6 +167,22 @@ $(document).bind("contextmenu",function(e){
     $("body").append(newframe);
   })
   
+// var city="Delhi";
+
+
+// function get(city){
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c371e679253980ec1ec3b83a05d197d5')
+// .then(response => response.json())
+// .then((data)=>{
+// console.log(data);
+// }
+
+// )
+
+// .catch(err => alert("Wrong city name!"));
+// }
+// get(city);
+  
 
 
 
