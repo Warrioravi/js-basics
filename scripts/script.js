1c588b5028ac160b1b1dcad28d39f91b9fd5b90d
Warrioravi <abhisinghraj2k18@gmail.com>
2021-08-05 01:24:44 +0530

    notification-bar-added

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,18 @@ tippy('.wifi', {
     content: `Battery? why you need to know that?:D`,
   });  
 
-document.querySelector(".name").addEventListener('click',(e)=>{
-    e.currentTarget.innerText="";
-})
-document.querySelector(".continue").addEventListener('click',(e)=>{
-    document.documentElement.requestFullscreen()
-    .then(()=>{
-        $(".userRegister").fadeOut("slow");
-        setTimeout(() => {
-            $(".bod").fadeOut("slow");
-        },10000);
-    })
-})
+// document.querySelector(".name").addEventListener('click',(e)=>{
+//     e.currentTarget.innerText="";
+// })
+// document.querySelector(".continue").addEventListener('click',(e)=>{
+//     document.documentElement.requestFullscreen()
+//     .then(()=>{
+//         $(".userRegister").fadeOut("slow");
+//         setTimeout(() => {
+//             $(".bod").fadeOut("slow");
+//         },10000);
+//     })
+// })
 
 $("#start").click(function(){
      $(".startmenu")[0].classList.toggle('visible');
@@ -173,27 +173,40 @@ $(document).bind("contextmenu",function(e){
     $(".controlmenu-slidebar")[0].classList.toggle("controlmenu-slidebar-visible")
   });
 
-  document.querySelector(".searchBar").addEventListener('click',()=>{
+
+  $(".searchBar").click(function(){
     document.querySelector(".search-slidebar").classList.toggle("search-slidebar-visible");
   })
 
-  // $(".searchBar")[0].click(function(){
-  //   console.log("searchbar clicked");
-  //   // $(".search-slidebar")[0].classList.toggle("search-slidebar-visible")
-  // })
-
-  // })
-  // $(".news-slidebar").click(function(){
-  //   $(".news-slidebar")[0].classList.toggle("news-slidebar-visible")
-
-  // })
-  // $(".notification-sliderbar")[0].click(function(){
-  //   $(this).classList.toggle("notification-slidebar-visible")
-
-  // })
+  $(".wifi").click(function(){
+    document.querySelector(".wifi-slidebar").classList.toggle("wifi-slidebar-visible");
+  })
+  $(".notifications").click(function(){
+    document.querySelector(".notification-sliderbar").classList.toggle("notification-slidebar-visible");
+  })
 
 
+  
+
+ document.querySelector(".weather").addEventListener('click',()=>{
+   document.querySelector('.news-slidebar').classList.toggle("news-slidebar-visible");
+ })
+ document.querySelector(".calender").addEventListener('click',()=>{
+  document.querySelector('.calender-slidebar').classList.toggle("calender-slidebar-visible");
+})
+
+
+  
+
+
+ ////code for the brightness control
+let rangeInput = document.getElementById('range');
  
+let container = document.querySelector("body");
+ 
+rangeInput.addEventListener("mousemove",function(){
+container.style.filter = "brightness(" + rangeInput.value + "%)";
+});
 
  
   
@@ -212,3 +225,168 @@ $(document).bind("contextmenu",function(e){
 // .catch(err => alert("Wrong city name!"));
 // }
 // get(city);
+
+
+
+
+
+//calender code
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
+
+
+
+//TIME AND DATE 
+
+  let today = new Date();
+  let t = ("0"+today.getHours()).slice(-2) + ":" + ("0"+today.getMinutes()).slice(-2);
+  let d = ("0"+today.getDate()).slice(-2)+'-'+("0"+today.getMonth()).slice(-2)+'-'+today.getFullYear();
+  document.querySelector(".calender").innerHTML=`${t}<br>${d}`;
+
+// cede for weather and news tiles 
+fetch("https://newsapi.org/v2/top-headlines?apiKey=a1f181aa894c4ebcbf3a17b37a4d9c5f&country=in")
+.then(res => res.json())
+.then((res)=>{
+  processNewsResponse(res);
+    console.log(res["articles"][0]["title"]);
+})
+.catch((e)=>{
+    console.log(e);
+})
+
+function processNewsResponse(res){
+  let newsTiles=document.querySelectorAll(".slidebar-news-tile");
+  for(let i=0;i<newsTiles.length;i++){
+    newsTiles[i].innerHTML=`<div>${res["articles"][i+4]["title"]}</div>`;
+    newsTiles[i].style.backgroundImage = `url(${res["articles"][i+4]["urlToImage"]})`;
+   }
+}
+
+//weather api call
+let addresslocation='Delhi';
+const api = {
+  key: "c371e679253980ec1ec3b83a05d197d5",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.location-name');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.innerText);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  let temp = document.querySelector(".lw-temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  document.querySelector("#taskbar-temperature").innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  
+  let iconcode = weather.weather[0].icon;
+  let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+  document.querySelector("#weather-icon").setAttribute('src',iconurl);
+  document.querySelector("#weather-taskbar-icon").setAttribute('src',iconurl);
+  
+  let weather_el = document.querySelector('.lw-description');
+  weather_el.innerText = weather.weather[0].main;
+  let weather_el2 = document.querySelector('#taskbar-weathertype');
+  weather_el2.innerText = weather.weather[0].main;
+
+
+
+
+}
+
+
+
+
+
+
